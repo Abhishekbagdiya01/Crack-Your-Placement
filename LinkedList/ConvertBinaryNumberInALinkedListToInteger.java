5467eb4af1f3becd00597a722e42d04dcefaf2e4
Abhishek bagdiya <abhishekbagdiya94@gmail.com>
2024-08-06 22:07:43 +0530

    Binary to decimal in linkedlist

---
+++ LinkedList/ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,24 @@
+/**
+ * ConvertBinaryNumberInALinkedListToInteger
+ */
+public class ConvertBinaryNumberInALinkedListToInteger {
+  // https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+  public static int getDecimalValue(ListNode head) {
+    ListNode current = head;
+    String sum = "";
+    while (current != null) {
+      sum += Integer.toString(current.val);
+      current = current.next;
+    }
+
+    int decimal = Integer.parseInt(sum, 2);
+    return decimal;
+  }
+
+  public static void main(String[] args) {
+    ListNode listNode = new ListNode(1,
+        new ListNode(0, new ListNode(1, new ListNode(0, new ListNode(1, new ListNode(0))))));
+    int a = getDecimalValue(listNode);
+    System.err.println(a);
+  }
+}
