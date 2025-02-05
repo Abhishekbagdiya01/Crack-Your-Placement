1e48890b4d430b1ada081f2970db1294ada94915
Abhishek bagdiya <abhishekbagdiya94@gmail.com>
2025-02-05 21:48:49 +0530

    Longest Consecutive Seq using Array method

--- Arrays/LongestConsecutive.java
+++ Arrays/LongestConsecutive.java
@@ -2,10 +2,10 @@
 /**
  * LongestConsecutive
  */
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class LongestConsecutive {
-
   public static int longestConsecutive(int[] nums) {
     HashSet<Integer> numSet = new HashSet<>();
     for (int num : nums) {
@@ -29,8 +29,29 @@ public class LongestConsecutive {
     return longest;
   }
 
+  public static int longestConsecutive1(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+
+    Arrays.sort(nums);
+    int lcs = 1;
+    int curr = 1;
+    for (int i = 1; i < nums.length; i++) {
+      int diff = nums[i] - nums[i - 1];
+      if (diff == 1) {
+        curr += 1;
+      } else {
+        curr = 1;
+      }
+      lcs = Math.max(curr, lcs);
+    }
+    return lcs;
+  }
+
   public static void main(String[] args) {
     int[] nums = { 100, 4, 200, 1, 3, 2 };
-    System.out.println("Longest Consecutive is " + longestConsecutive(nums));
+    System.out.println("Method 1 Longest Consecutive is " + longestConsecutive(nums));
+    System.out.println("Method 2 Longest Consecutive is " + longestConsecutive1(nums));
   }
 }
